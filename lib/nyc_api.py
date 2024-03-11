d8a810d9631d3fbe57ecf9529a97ade8ab544cf9
vchemu <valerine.chemutai@student.moringaschool.com>
2024-03-11 10:28:45 +0300

    get

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -9,6 +9,18 @@ class GetPrograms:
 
     response = requests.get(URL)
     return response.content
+  
+  def program_school(self):
+    programs_list = []
+    programs = json.loads(self.get_programs())
+    for program in programs:
+      programs_list.append(program["agency"])
 
-programs = GetPrograms().get_programs()
-print(programs)
+    return programs_list
+
+#programs = GetPrograms().get_programs()
+#print(programs)
+programs = GetPrograms()
+programs_schools = programs.program_school()
+for school in set(programs_schools):
+  print(school)
